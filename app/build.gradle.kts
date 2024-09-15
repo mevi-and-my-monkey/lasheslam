9bc7c5fb455c5132b0301e009445d4d495775d53
AlejandroMevi <106292726+AlejandroMevi@users.noreply.github.com>
2024-09-15 15:01:00 -0600

    implementacion de librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-parcelize")
+    id("com.google.gms.google-services")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -33,6 +37,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +52,47 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.app.update.ktx)
+    implementation(libs.androidx.material3)
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
+    implementation(libs.app.update.ktx)
+    // Fragment
+    implementation(libs.androidx.fragment.ktx)
+    // Activity
+    implementation(libs.androidx.activity.ktx)
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    //Corrutinas
+    implementation(libs.kotlinx.coroutines.android)
+    //recyclerView
+    implementation(libs.androidx.recyclerview)
+    //For control over item selection of both touch and mouse driven selection
+    implementation(libs.androidx.recyclerview.selection)
+    //Imagen circular
+    implementation(libs.circleimageview)
+    //Navigation Fragment
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    //swipe
+    implementation(libs.androidx.swiperefreshlayout)
+    //progress shimmer
+    implementation(libs.shimmer)
+    //Google maps
+    implementation(libs.play.services.maps)
+    //Glide
+    implementation(libs.glide)
+    //firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    // For control over item selection of both touch and mouse driven selection
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+    //Dagger Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+}
+kapt {
+    correctErrorTypes = true
 }
